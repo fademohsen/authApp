fe61cf433bbfda1d00e2aa184d9b6671ef9be380
Fadi Mohsen <xfade@outlook.com>
2020-07-06 00:31:37 +0300

    fix

--- models/shared.js
+++ models/shared.js
@@ -1,6 +1,8 @@
 var Product = require("./product");
 var User = require("./user");
 const Cart = require("./cart");
+const Country = require("./country");
+const City = require("./city");
 
 async function productsCount() {
   return Product.count((err, count) => {
@@ -20,33 +22,44 @@ async function getCartCount(id) {
   });
 }
 
-async function getCard(id) {
-  return Cart.find({user : id} , (err , result) => {
-    console.log("result" , result);
-    console.log(err);
-    
-    
-
-    return result
-  }).populate('product')
-}
-async function sumCart(cart) {
-  return new Promise((resolve , regect) => {
-    var total = 0 ;
-    cart.forEach(c => {
-      total += c.product.price * c.count
-      
+async function getCountries() {
+  return new Promise((resolve, reject) => {
+    Country.find((err, result) => {
+      resolve(result);
     });
-    resolve(total) ;
-  })
- 
+  });
+}
+async function getCities(id) {
+  return new Promise((resolve, reject) => {
+    City.find({country : id} , (err, result) => {
+      resolve(result);
+    });
+  });
+}
 
+async function getCart(id) {
+  return Cart.find({ user: id }, (errr, result) => {
+    console.log("result cart", result);
+    return result;
+  }).populate("product");
+}
+
+async function sumCart(cart) {
+  return new Promise((resolve, reject) => {
+    var total = 0;
+    cart.forEach((c) => {
+      total += c.product.price * c.count;
+    });
+    resolve(total);
+  });
 }
 
 module.exports = {
   productsCount,
   usersCount,
-  getCartCount , 
-  getCard ,
-  sumCart
+  getCartCount,
+  getCart,
+  sumCart,
+  getCountries,
+  getCities,
 };
--- routes/cart.js
+++ routes/cart.js
@@ -3,24 +3,21 @@ var Cart = require("../models/cart");
 var router = express.Router();
 
 router.get("/add/:id", (req, res) => {
-  let id = req.session.passport.user._id ;
-  Cart.findOne({user : id , product: req.params.id} , (err , cart)=> {
-    if(cart == null) {
+  let id = req.session.passport.user._id;
+  Cart.findOne({user : id , product : req.params.id} , (err, cart)=>{
+    if (cart == null) {
       Cart.create({
         user: id,
-        product: req.params.id ,
+        product: req.params.id,
       }).then((result) => {
         res.redirect("/");
       });
-
     }else{
-      cart.count++; 
-      Cart.updateOne({user : id , product : req.params.id} , cart , (err , result) => {
-        res.redirect('/')
+      cart.count++;
+      Cart.updateOne({user : id , product : req.params.id} , cart , (err , result)=>{
+        res.redirect('/');
       })
-
     }
-
   })
   
 });
